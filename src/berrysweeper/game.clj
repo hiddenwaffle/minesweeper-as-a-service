b5390986f7b7f179d1e0e9e67a8eeaa1e63ea5e7
Anthony Villena <unremarkableSCM@users.noreply.github.com>
2018-10-27 11:33:35 -0500

    Add game handlers for reset and pick

---
+++ src/berrysweeper/game.clj
@@ -0,0 +1,7 @@
+(ns berrysweeper.game)
+
+(defn reset []
+  {:reset "as-json"})
+
+(defn pick [old-state]
+  {:pick "as-json"})
--- src/berrysweeper/handler.clj
+++ src/berrysweeper/handler.clj
@@ -1,11 +1,29 @@
 (ns berrysweeper.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
+
+(defn reset-handler [request]
+  (let [body (berrysweeper.game/reset)]
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body body}))
+
+(defn pick-handler [request]
+  (let [body (berrysweeper.game/pick (:body request))]
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body body}))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] "")
+  (GET "/reset" [] reset-handler)
+  (POST "/pick" [] pick-handler)
+  (route/not-found ""))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
