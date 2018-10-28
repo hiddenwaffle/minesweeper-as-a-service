acae8025cd271b1a71c4d5e07a4dd5b35c088559
Anthony Villena <unremarkableSCM@users.noreply.github.com>
2018-10-27 23:50:31 -0500

    Add reset functionality

--- src/minesweeper_saas/game.clj
+++ src/minesweeper_saas/game.clj
@@ -1,11 +1,20 @@
 (ns minesweeper-saas.game)
 
-(def board-height 8)
-(def board-width 8)
-(def starting-mines-count 10)
+(defn cartesian [xs ys]
+  (#(for [x %1 y %2] [x y]) xs ys))
+
+(defn generate-mines []
+  (let [board-height 8
+        board-width 8
+        starting-mines-count 10
+        board-x-positions (range 0 board-width)
+        board-y-positions (range 0 board-height)]
+    (take starting-mines-count
+          (shuffle (cartesian board-x-positions
+                              board-y-positions)))))
 
 (defn reset []
-  {:reset "as-json"})
+  {:mines (generate-mines)})
 
 (defn pick [old-state]
   {:pick "as-json"})
